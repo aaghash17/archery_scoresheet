3f401465ebfbb71ff1f592a9a98622842086a936
Aaghash R <aaghash.r@in.abb.com>
2024-08-29 19:12:41 +0530

    Export order changed

--- src/components/ExportCSV.jsx
+++ src/components/ExportCSV.jsx
@@ -2,6 +2,47 @@ import Papa from "papaparse";
 import { ref, get } from "firebase/database";
 import { db } from "../firebase/firebaseConfig";
 
+const columnOrder = [
+  "tboard",
+  "tplayer",
+  "pname",
+  "district",
+  "age",
+  "sex",
+  "bow",
+  "total",
+  "d11",
+  "d12",
+  "d13",
+  "d21",
+  "d22",
+  "d23",
+  "d31",
+  "d32",
+  "d33",
+  "d41",
+  "d42",
+  "d43",
+  "d51",
+  "d52",
+  "d53",
+  "d61",
+  "d62",
+  "d63",
+  "d71",
+  "d72",
+  "d73",
+  "d81",
+  "d82",
+  "d83",
+  "d91",
+  "d92",
+  "d93",
+  "d101",
+  "d102",
+  "d103",
+];
+
 const ExportCSV = () => {
   const handleExport = () => {
     const dataRef = ref(db, "data");
@@ -12,13 +53,23 @@ const ExportCSV = () => {
           const data = snapshot.val();
           const formattedData = Object.keys(data).map((key) => {
             const item = data[key];
-            // Remove the 'id' field
+            // Remove the 'id' field if it exists
             delete item.id;
-            return item;
+
+            // Rearrange the columns based on columnOrder
+            const orderedItem = {};
+            columnOrder.forEach((col) => {
+              orderedItem[col] = item[col] || ""; // Default to empty string if the column is missing
+            });
+
+            return orderedItem;
           });
 
           // Convert JSON to CSV
-          const csv = Papa.unparse(formattedData);
+          const csv = Papa.unparse(formattedData, {
+            header: true,
+            columns: columnOrder, // Specify column order
+          });
 
           // Create a downloadable link
           const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
