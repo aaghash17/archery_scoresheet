42e2c2d9f55e114aec0b6711744e26578d7c6f88
aaghash17 <aaghash17@gmail.com>
2024-09-03 19:11:55 +0530

    Adjusted css for data table

--- src/components/Score/ScoreTable.jsx
+++ src/components/Score/ScoreTable.jsx
@@ -13,95 +13,80 @@ function ScoreTable({ scores, onScoreChange }) {
 
   return (
     <div className="table-scoresheet">
-      <div className="table-heading">
-        <div className="table-heading1">
-          <div className="frame-1">
-            <div className="_text">S.No</div>
-          </div>
-          <div className="frame-2">
-            <div className="_text">Score</div>
-          </div>
-          <div className="frame-3">
-            <div className="_text">Sum</div>
+      <div className="table-heading1">
+        <div className="table-heading1-frame-1">
+          <div className="_text">S.No</div>
+        </div>
+        <div className="table-heading1-frame-2">
+          <div className="_text">Score</div>
+        </div>
+        <div className="table-heading1-frame-3">
+          <div className="_text">Sum</div>
+        </div>
+      </div>
+      <div className="table-heading2">
+        <div className="table-heading2-frame-1">
+          <div className="_text">1</div>
+        </div>
+        <div className="table-heading2-frame-1">
+          <div className="_text">2</div>
+        </div>
+        <div className="table-heading2-frame-1">
+          <div className="_text">3</div>
+        </div>
+      </div>
+      <div className="row-data">
+        <div className="c-sno">
+          <div className="_text">1</div>
+        </div>
+        <div className="c-1">
+          <input
+            type="number"
+            id="d11"
+            value={scores.d11 || ""}
+            onChange={onScoreChange("d11")}
+          />
+        </div>
+        <div className="c-2">
+          <input
+            type="number"
+            id="d12"
+            value={scores.d12 || ""}
+            onChange={onScoreChange("d12")}
+          />
+        </div>
+        <div className="c-3">
+          <input
+            type="number"
+            id="d13"
+            value={scores.d13 || ""}
+            onChange={onScoreChange("d13")}
+          />
+        </div>
+        <div className="c-sum">
+          <div className="_text">
+            <label id="s1">{calculateSum1}</label>
           </div>
         </div>
-        <div className="table-heading2">
-          <div className="frame-4">
-            <div className="_text">1</div>
-          </div>
-          <div className="frame-4">
-            <div className="_text">2</div>
-          </div>
-          <div className="frame-4">
-            <div className="_text">3</div>
-          </div>
+      </div>
+      <div className="row-data">
+        <div className="c-sno">
+          <div className="_text">2</div>
         </div>
-        <div className="table-data">
-          <div className="row-1">
-            <div className="c-sno">
-              <div className="_text">1</div>
-            </div>
-            <div className="c-1">
-              <input
-                type="number"
-                id="d11"
-                value={scores.d11 || ""}
-                onChange={onScoreChange("d11")}
-              />
-            </div>
-            <div className="c-2">
-              <input
-                type="number"
-                id="d12"
-                value={scores.d12 || ""}
-                onChange={onScoreChange("d12")}
-              />
-            </div>
-            <div className="c-3">
-              <input
-                type="number"
-                id="d13"
-                value={scores.d13 || ""}
-                onChange={onScoreChange("d13")}
-              />
-            </div>
-            <div className="c-sum">
-              <div className="_text">
-                <label id="s1">{calculateSum1}</label>
-              </div>
-            </div>
-          </div>
-          <div className="row-2">
-            <div className="c-sno">
-              <div className="_text">2</div>
-            </div>
-            <div className="c-1"></div>
-            <div className="c-2"></div>
-            <div className="c-3"></div>
-            <div className="c-sum">
-              <div className="_text"></div>
-            </div>
-          </div>
-          <div className="row-3">
-            <div className="c-sno">
-              <div className="_text">3</div>
-            </div>
-            <div className="c-1"></div>
-            <div className="c-2"></div>
-            <div className="c-3"></div>
-            <div className="c-sum">
-              <div className="_text"></div>
-            </div>
-          </div>
+        <div className="c-1"></div>
+        <div className="c-2"></div>
+        <div className="c-3"></div>
+        <div className="c-sum">
+          <div className="_text"></div>
         </div>
-        <div className="table-total">
-          <div className="frame-18">
-            <div className="_text">Total</div>
-          </div>
-          <div className="frame-19">
-            <div className="_text">
-              <label id="total"></label>
-            </div>
+      </div>
+      <div className="table-total">
+        <div className="table-total-frame-1">
+          <div className="_text">Total</div>
+        </div>
+        <div className="table-total-frame-2">
+          <div className="_text">
+            <label id="total"></label>
           </div>
         </div>
       </div>
